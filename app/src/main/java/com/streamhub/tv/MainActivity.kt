33c7cac1962dd28a99757184d893827030ae0686
tkmntysn-creator <tkmntysn@gmail.com>
2026-07-30 08:34:33 +0000

    Fix imports: WindowSizeClass annotation path and missing getValue delegate

--- app/src/main/java/com/streamhub/tv/MainActivity.kt
+++ app/src/main/java/com/streamhub/tv/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
--- app/src/main/java/com/streamhub/tv/ui/navigation/AdaptiveScaffold.kt
+++ app/src/main/java/com/streamhub/tv/ui/navigation/AdaptiveScaffold.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
