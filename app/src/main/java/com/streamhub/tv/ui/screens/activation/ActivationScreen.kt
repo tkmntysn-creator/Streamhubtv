dd1ae6dedee496db373c4141a08b44ec07fd234f
tkmntysn-creator <tkmntysn@gmail.com>
2026-08-11 00:59:36 +0100

    Add size import to ActivationScreen.kt

--- app/src/main/java/com/streamhub/tv/ui/screens/activation/ActivationScreen.kt
+++ app/src/main/java/com/streamhub/tv/ui/screens/activation/ActivationScreen.kt
@@ -1,6 +1,7 @@
 package com.streamhub.tv.ui.screens.activation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
